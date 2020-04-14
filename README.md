9edecf23a2d64b18b0be84fa73ac0e3195be2121
tsadio <tsadio@montgomerycollege.edu>
2020-04-14 11:57:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Another Data Type Variables" 
---
+++ src/AnotherDataTypeVariables.java
@@ -0,0 +1,22 @@
+public class AnotherDataTypeVariables {
+    public static void main (String[] args)  {
+        boolean flag = true;
+        byte b = 4;
+        char a = 'A';
+        short index = 3;
+        int i = 89;
+        float f = 4.73f;
+        long longVar = 1234567890;
+        double doubleVar = 123456789.123456789;
+
+        System.out.println("flag: " + flag);
+        System.out.println("byte: " + b);
+        System.out.println("char: " + a);
+        System.out.println("short: " + index);
+        System.out.println("integer: " + i);
+        System.out.println("float: " + f);
+        System.out.println("long: " + longVar);
+        System.out.println("double: " + doubleVar);
+    }
+
+}
